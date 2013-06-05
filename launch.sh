9ef812a672b4345460f312fbcfc03702bf02736f
hjeldin <arena.riccardo@live.it>
2013-06-05 18:18:44 +0200

    added launch script

---
+++ launch.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+SERVERIP=localhost
+PROJ_DIR=/home/hjeldin/projects/glowing-tyrion/
+CODEBASE_DIR=http://$SERVERIP:8000/common/
+POLICY_DIR=/home/hjeldin/public_html/common
+LOG_DIR=/home/hjeldin/public_html/log
+
+export CLASSPATH=$POLICY_DIR
+
+sudo pkill rmiregistry
+sudo pkill tnameserv
+#sudo pkill python
+sudo pkill rmid
+sudo pkill java
+
+sleep 1
+
+rmiregistry 2222 &
+tnameserv -ORBInitialPort 5555 &
+
+sleep 1
+
+cd $POLICY_DIR/../
+#python -m SimpleHTTPServer &
+
+cd ~
+rmid -log $LOG_DIR -J-Djava.rmi.server.codebase=$CODEBASE_DIR -J-Djava.security.policy=$POLICY_DIR/rmid.policy -port 5551 &
+
+sleep 1
+
+cd $PROJ_DIR
+
+pwd
+
+java -Djava.security.policy=$POLICY_DIR/rmid.policy -Djavax.net.ssl.trustStore=$PROJ_DIR/server.keystore \
+		 -Djavax.net.ssl.keyStore=$PROJ_DIR/server.keystore -Djavax.net.ssl.trustStorePassword=server \
+		 -Djavax.net.ssl.keyStorePassword=server -Djava.rmi.server.codebase=$CODEBASE_DIR \
+		 -classpath :bin servers.RmiRegistry &
+
+sleep 1
+
+cd $PROJ_DIR
+
+java -classpath :bin/:lib/gson-2.2.3.jar -Djava.rmi.server.codebase=$CODEBASE_DIR \
+		 -Dservers.impl.codebase=$CODEBASE_DIR -Djava.security.policy=$POLICY_DIR/rmid.policy \
+	   -Dservers.classeserver=servers.GameServer -Dservers.policy=$POLICY_DIR/rmid.policy \
+	   -Djavax.net.ssl.trustStore=$PROJ_DIR/server.keystore -Djavax.net.ssl.keyStore=$PROJ_DIR/server.keystore \
+	   -Djavax.net.ssl.keyStorePassword=server -Djava.rmi.activation.port=5551 \
+	   -Djavax.net.ssl.trustStorePassword=server servers.SetupGame &
+
+sleep 1
+
+cd $PROJ_DIR
+
+java -classpath :bin/ -Djava.rmi.server.codebase=$CODEBASE_DIR -Djava.rmi.codebase=$CODEBASE_DIR -Dservers.impl.codebase=$CODEBASE_DIR \
+     -Djava.security.policy=$POLICY_DIR/rmid.policy -Dservers.classeserver=servers.LoginServer \
+     -Dservers.policy=$POLICY_DIR/rmid.policy -Djavax.net.ssl.trustStore=$PROJ_DIR/server.keystore \
+     -Djavax.net.ssl.keyStore=$PROJ_DIR/server.keystore -Djavax.net.ssl.keyStorePassword=server -Djava.rmi.activation.port=5551 \
+     servers.SetupLogin &
+
+sleep 1
+
+cd $PROJ_DIR
+
+java -Djava.security.policy=$POLICY_DIR/rmid.policy -Djava.rmi.server.codebase=$CODEBASE_DIR \
+		 -Djavax.net.ssl.trustStore=$PROJ_DIR/server.keystore \
+		 -Djavax.net.ssl.keyStore=$PROJ_DIR/server.keystore -Djavax.net.ssl.trustStorePassword=server -Djavax.net.ssl.keyStorePassword=server \
+		 -classpath :bin/ proxies.ProxyServer
